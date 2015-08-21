c04f029f380da0730fe4c6410bd8b90414b3578a
Anders Pearson <anders@columbia.edu>
2015-08-21 12:25:40 +0200

    :arrow_up: django 1.8.4
    nuked the fixture-based tests. Will replace that coverage with faster
    factory based tests next.

--- countryx/settings_shared.py
+++ countryx/settings_shared.py
@@ -21,6 +21,7 @@ DATABASES = {
         'PORT': 5432,
         'USER': '',
         'PASSWORD': '',
+        'ATOMIC_REQUESTS': True,
     }
 }
 
@@ -33,6 +34,7 @@ if 'test' in sys.argv or 'jenkins' in sys.argv:
             'PORT': '',
             'USER': '',
             'PASSWORD': '',
+            'ATOMIC_REQUESTS': True,
         }
     }
 
@@ -60,10 +62,10 @@ TEMPLATE_LOADERS = (
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
-    'django.core.context_processors.debug',
-    'django.core.context_processors.request',
+    'django.template.context_processors.debug',
+    'django.template.context_processors.request',
     'stagingcontext.staging_processor',
-    'django.core.context_processors.static',
+    'django.template.context_processors.static',
     'djangowind.context.context_processor',
     'gacontext.ga_processor',
 )
@@ -76,7 +78,6 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
-    'django.middleware.transaction.TransactionMiddleware',
     'waffle.middleware.WaffleMiddleware',
     'impersonate.middleware.ImpersonateMiddleware',
     'countryx.sim.middleware.GameStateMiddleware',
