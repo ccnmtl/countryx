60fa796d909fc3b72c76c27b5f404abc1b85b444
Anders Pearson <anders@columbia.edu>
2015-08-26 11:26:02 +0200

    add some view tests back in

---
+++ countryx/sim/tests/test_views.py
@@ -0,0 +1,70 @@
+from django.test import TestCase, RequestFactory, Client
+
+from .factories import UserFactory, StateFactory
+from countryx.sim.views import (
+    root, allpaths, allquestions, allvariables
+)
+
+
+class RootTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_player(self):
+        request = self.factory.get("/")
+        request.user = UserFactory()
+        response = root(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_faculty(self):
+        request = self.factory.get("/")
+        request.user = UserFactory(is_staff=True)
+        response = root(request)
+        self.assertEqual(response.status_code, 200)
+
+
+class AllPathsTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_allpaths_empty(self):
+        request = self.factory.get("/")
+        request.user = UserFactory()
+        response = allpaths(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_allpaths_one_state(self):
+        StateFactory()
+        request = self.factory.get("/")
+        request.user = UserFactory()
+        response = allpaths(request)
+        self.assertEqual(response.status_code, 200)
+
+
+class AllQuestionsTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_allquestions_empty(self):
+        request = self.factory.get("/")
+        request.user = UserFactory()
+        response = allquestions(request)
+        self.assertEqual(response.status_code, 200)
+
+
+class AllVariablesTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_allpaths_empty(self):
+        request = self.factory.get("/")
+        request.user = UserFactory()
+        response = allvariables(request)
+        self.assertEqual(response.status_code, 200)
+
+
+class SmoketestTest(TestCase):
+    def test_smoketest(self):
+        c = Client()
+        r = c.get("/smoketest/")
+        self.assertEqual(r.status_code, 200)
